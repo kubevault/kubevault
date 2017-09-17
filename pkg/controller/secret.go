a60c5fb03971d6fcade6d174920aed74a3fcbece
Tamal Saha <tamal@appscode.com>
2017-09-17 00:39:53 -0700

    Sync secrets to vault (#7)

--- pkg/controller/secret.go
+++ pkg/controller/secret.go
@@ -2,10 +2,13 @@ package controller
 
 import (
 	"fmt"
+	"path"
+	"time"
 
 	"github.com/golang/glog"
 	"k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/pkg/api/v1"
+	"k8s.io/client-go/tools/cache"
 )
 
 func (c *VaultController) runSecretWatcher() {
@@ -25,7 +28,7 @@ func (c *VaultController) processNextSecret() bool {
 	defer c.sQueue.Done(key)
 
 	// Invoke the method containing the business logic
-	err := c.syncSToStdout(key.(string))
+	err := c.syncSecretToVault(key.(string))
 	if err == nil {
 		// Forget about the #AddRateLimited history of the key on every successful synchronization.
 		// This ensures that future processing of updates for this key is not delayed because of
@@ -54,20 +57,44 @@ func (c *VaultController) processNextSecret() bool {
 // syncToStdout is the business logic of the controller. In this controller it simply prints
 // information about the secret to stdout. In case an error happened, it has to simply return the error.
 // The retry logic should not be part of the business logic.
-func (c *VaultController) syncSToStdout(key string) error {
+func (c *VaultController) syncSecretToVault(key string) error {
 	obj, exists, err := c.sIndexer.GetByKey(key)
 	if err != nil {
 		glog.Errorf("Fetching object with key %s from store failed with %v", key, err)
 		return err
 	}
-
 	if !exists {
 		// Below we will warm up our cache with a Secret, so that we will see a delete for one secret
 		fmt.Printf("Secret %s does not exist anymore\n", key)
+
+		namespace, name, err := cache.SplitMetaNamespaceKey(key)
+		if err != nil {
+			return err
+		}
+		c.vaultClient.Logical().Delete(path.Join(c.options.SecretBackend(), namespace, name))
 	} else {
-		// Note that you also have to check the uid if you have a local controlled resource, which
-		// is dependent on the actual instance, to detect that a Secret was recreated with the same name
-		fmt.Printf("Sync/Add/Update for Secret %s\n", obj.(*v1.Secret).GetName())
+		secret := obj.(*v1.Secret)
+		fmt.Printf("Sync/Add/Update for Secret %s\n", secret.GetName())
+
+		data := map[string]interface{}{}
+		for k, v := range secret.Data {
+			data[k] = string(v)
+		}
+		data["m:uid"] = secret.ObjectMeta.UID
+		data["m:resourceVersion"] = secret.ObjectMeta.ResourceVersion
+		data["m:generation"] = secret.ObjectMeta.Generation
+		data["m:creationTimestamp"] = secret.ObjectMeta.CreationTimestamp.UTC().Format(time.RFC3339)
+		if secret.ObjectMeta.DeletionTimestamp != nil {
+			data["m:deletionTimestamp"] = secret.ObjectMeta.DeletionTimestamp.UTC().Format(time.RFC3339)
+		}
+		for k, v := range secret.ObjectMeta.Labels {
+			data["l:"+k] = v
+		}
+		for k, v := range secret.ObjectMeta.Annotations {
+			data["a:"+k] = v
+		}
+		_, err = c.vaultClient.Logical().Write(path.Join(c.options.SecretBackend(), secret.Namespace, secret.Name), data)
+		return err
 	}
 	return nil
 }
