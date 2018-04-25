e7435391403f9b1bb0a3474c209c4dc91f890615
Md. Nure Alam Nahid <nahid@appscode.com>
2018-04-25 12:04:48 +0600

    Add vault api (#45)

--- apis/vault/v1alpha1/types.go
+++ apis/vault/v1alpha1/types.go
@@ -11,6 +11,13 @@ const (
 	ResourcePluralVaultServer   = "vaultservers"
 )
 
+type ClusterPhase string
+
+const (
+	ClusterPhaseInitial ClusterPhase = ""
+	ClusterPhaseRunning              = "Running"
+)
+
 // +genclient
 // +genclient:skipVerbs=updateStatus
 // +k8s:openapi-gen=true
@@ -19,25 +26,33 @@ const (
 type VaultServer struct {
 	metav1.TypeMeta   `json:",inline,omitempty"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
-	Spec              VaultServerSpec `json:"spec,omitempty"`
+	Spec              VaultServerSpec   `json:"spec,omitempty"`
+	Status            VaultServerStatus `json:"status",omitempty`
 }
 
 type VaultServerSpec struct {
-	Selector metav1.LabelSelector `json:"selector,omitempty"`
-	Schedule string               `json:"schedule,omitempty"`
-	// Pod volumes to mount into the sidecar container's filesystem.
-	VolumeMounts []core.VolumeMount `json:"volumeMounts,omitempty"`
-	// Compute Resources required by the sidecar container.
-	Resources core.ResourceRequirements `json:"resources,omitempty"`
-	//Indicates that the VaultServer is paused from taking backup. Default value is 'false'
-	// +optional
-	Paused bool `json:"paused,omitempty"`
-	// ImagePullSecrets is an optional list of references to secrets in the same namespace to use for pulling any of the images used by this PodSpec.
-	// If specified, these secrets will be passed to individual puller implementations for them to use. For example,
-	// in the case of docker, only DockerConfig type secrets are honored.
-	// More info: https://kubernetes.io/docs/concepts/containers/images#specifying-imagepullsecrets-on-a-pod
-	// +optional
-	ImagePullSecrets []core.LocalObjectReference `json:"imagePullSecrets,omitempty"`
+	// Number of nodes to deploy for a Vault deployment.
+	// Default: 1.
+	Nodes int32 `json:"nodes,omitempty"`
+
+	// Base image to use for a Vault deployment.
+	BaseImage string `json:"baseImage"`
+
+	// Version of Vault to be deployed.
+	Version string `json:"version"`
+
+	// Pod defines the policy for pods owned by vault operator.
+	// This field cannot be updated once the CR is created.
+	Pod *PodPolicy `json:"pod,omitempty"`
+
+	// Name of the ConfigMap for Vault's configuration
+	// If this is empty, operator will create a default config for Vault.
+	// If this is not empty, operator will create a new config overwriting
+	// the "storage", "listener" sections in orignal config.
+	ConfigMapName string `json:"configMapName"`
+
+	// TLS policy of vault nodes
+	TLS *TLSPolicy `json:"TLS,omitempty"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
@@ -46,4 +61,75 @@ type VaultServerList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
 	Items           []VaultServer `json:"items,omitempty"`
+}
+
+type VaultServerStatus struct {
+	// Phase indicates the state this Vault cluster jumps in.
+	// Phase goes as one way as below:
+	//   Initial -> Running
+	Phase ClusterPhase `json:"phase"`
+
+	// Initialized indicates if the Vault service is initialized.
+	Initialized bool `json:"initialized"`
+
+	// ServiceName is the LB service for accessing vault nodes.
+	ServiceName string `json:"serviceName,omitempty"`
+
+	// ClientPort is the port for vault client to access.
+	// It's the same on client LB service and vault nodes.
+	ClientPort int `json:"clientPort,omitempty"`
+
+	// VaultStatus is the set of Vault node specific statuses: Active, Standby, and Sealed
+	VaultStatus VaultStatus `json:"vaultStatus"`
+
+	// PodNames of updated Vault nodes. Updated means the Vault container image version
+	// matches the spec's version.
+	UpdatedNodes []string `json:"updatedNodes,omitempty"`
+}
+
+type VaultStatus struct {
+	// PodName of the active Vault node. Active node is unsealed.
+	// Only active node can serve requests.
+	// Vault service only points to the active node.
+	Active string `json:"active"`
+
+	// PodNames of the standby Vault nodes. Standby nodes are unsealed.
+	// Standby nodes do not process requests, and instead redirect to the active Vault.
+	Standby []string `json:"standby"`
+
+	// PodNames of Sealed Vault nodes. Sealed nodes MUST be manually unsealed to
+	// become standby or leader.
+	Sealed []string `json:"sealed"`
+}
+
+// PodPolicy defines the policy for pods owned by vault operator.
+type PodPolicy struct {
+	// Resources is the resource requirements for the containers.
+	Resources core.ResourceRequirements `json:"resources,omitempty"`
+}
+
+// TLSPolicy defines the TLS policy of the vault nodes
+type TLSPolicy struct {
+	// StaticTLS enables user to use static x509 certificates and keys,
+	// by putting them into Kubernetes secrets, and specifying them here.
+	// If this is not set, operator will auto-gen TLS assets and secrets.
+	Static *StaticTLS `json:"static,omitempty"`
+}
+
+type StaticTLS struct {
+	// ServerSecret is the secret containing TLS certs used by each vault node
+	// for the communication between the vault server and its clients.
+	// The server secret should contain two files: server.crt and server.key
+	// The server.crt file should only contain the server certificate.
+	// It should not be concatenated with the optional ca certificate as allowed by https://www.vaultproject.io/docs/configuration/listener/tcp.html#tls_cert_file
+	// The server certificate must allow the following wildcard domains:
+	// localhost
+	// *.<namespace>.pod
+	// <vault-cluster-name>.<namespace>.svc
+	ServerSecret string `json:"serverSecret,omitempty"`
+
+	// ClientSecret is the secret containing the CA certificate
+	// that will be used to verify the above server certificate
+	// The ca secret should contain one file: vault-client-ca.crt
+	ClientSecret string `json:"clientSecret,omitempty"`
 }
