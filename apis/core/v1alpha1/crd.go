886c566801d63c6869689aba0795c2e37b3ccc57
Tamal Saha <tamal@appscode.com>
2018-06-21 20:06:48 -0700

    Apply validation rules to vault server names

--- apis/core/v1alpha1/crd.go
+++ apis/core/v1alpha1/crd.go
@@ -2,8 +2,25 @@ package v1alpha1
 
 import (
 	crdutils "github.com/appscode/kutil/apiextensions/v1beta1"
+	"github.com/go-openapi/spec"
 	apiextensions "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	"k8s.io/kube-openapi/pkg/common"
 )
+
+func setNameSchema(openapiSpec map[string]common.OpenAPIDefinition) {
+	// ref: https://github.com/kubedb/project/issues/166
+	// https://github.com/kubernetes/apimachinery/blob/94ebb086c69b9fec4ddbfb6a1433d28ecca9292b/pkg/util/validation/validation.go#L153
+	var maxLength int64 = 63
+	openapiSpec["k8s.io/apimachinery/pkg/apis/meta/v1.ObjectMeta"].Schema.SchemaProps.Properties["name"] = spec.Schema{
+		SchemaProps: spec.SchemaProps{
+			Description: "Name must be unique within a namespace. Is required when creating resources, although some resources may allow a client to request the generation of an appropriate name automatically. Name is primarily intended for creation idempotence and configuration definition. Cannot be updated. More info: http://kubernetes.io/docs/user-guide/identifiers#names",
+			Type:        []string{"string"},
+			Format:      "",
+			Pattern:     `^[a-z]([-a-z0-9]*[a-z0-9])?$`,
+			MaxLength:   &maxLength,
+		},
+	}
+}
 
 func (c VaultServer) CustomResourceDefinition() *apiextensions.CustomResourceDefinition {
 	return crdutils.NewCustomResourceDefinition(crdutils.Config{
@@ -20,5 +37,5 @@ func (c VaultServer) CustomResourceDefinition() *apiextensions.CustomResourceDef
 		SpecDefinitionName:    "github.com/kube-vault/operator/apis/core/v1alpha1.VaultServer",
 		EnableValidation:      true,
 		GetOpenAPIDefinitions: GetOpenAPIDefinitions,
-	})
+	}, setNameSchema)
 }
