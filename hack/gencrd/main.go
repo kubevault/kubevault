b0a8e4260742949f8fa33fccfa8dc9816adcfaa2
Tamal Saha <tamal@appscode.com>
2018-08-02 18:30:05 -0700

    Move crds to api folder
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/gencrd/main.go
+++ hack/gencrd/main.go
@@ -23,18 +23,24 @@ import (
 
 func generateCRDDefinitions() {
 	filename := gort.GOPath() + "/src/github.com/kubevault/operator/apis/core/v1alpha1/crds.yaml"
+	os.Remove(filename)
 
-	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	err := os.MkdirAll(filepath.Join(gort.GOPath(), "/src/github.com/kubevault/operator/api/crds"), 0755)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer f.Close()
 
 	crds := []*crd_api.CustomResourceDefinition{
 		stashv1alpha1.VaultServer{}.CustomResourceDefinition(),
 	}
 	for _, crd := range crds {
+		filename := filepath.Join(gort.GOPath(), "/src/github.com/kubevault/operator/api/crds", crd.Spec.Names.Singular+".yaml")
+		f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
 		crdutils.MarshallCrd(f, crd, "yaml")
+		f.Close()
 	}
 }
 func generateSwaggerJson() {
