b770eececd1aec8884c2afb79907149e1f53d510
Tamal Saha <tamal@appscode.com>
2019-11-04 04:14:42 -0800

    Test installers (#223)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/dev/run.sh
+++ hack/dev/run.sh
@@ -23,37 +23,37 @@ pushd $REPO_ROOT
 
 # http://redsymbol.net/articles/bash-exit-traps/
 function cleanup() {
-  rm -rf $ONESSL ca.crt ca.key server.crt server.key
+    rm -rf $ONESSL ca.crt ca.key server.crt server.key
 }
 trap cleanup EXIT
 
 # https://stackoverflow.com/a/677212/244009
 if [[ ! -z "$(command -v onessl)" ]]; then
-  export ONESSL=onessl
+    export ONESSL=onessl
 else
-  # ref: https://stackoverflow.com/a/27776822/244009
-  case "$(uname -s)" in
-    Darwin)
-      curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.7.0/onessl-darwin-amd64
-      chmod +x onessl
-      export ONESSL=./onessl
-      ;;
+    # ref: https://stackoverflow.com/a/27776822/244009
+    case "$(uname -s)" in
+        Darwin)
+            curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.7.0/onessl-darwin-amd64
+            chmod +x onessl
+            export ONESSL=./onessl
+            ;;
 
-    Linux)
-      curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.7.0/onessl-linux-amd64
-      chmod +x onessl
-      export ONESSL=./onessl
-      ;;
+        Linux)
+            curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.7.0/onessl-linux-amd64
+            chmod +x onessl
+            export ONESSL=./onessl
+            ;;
 
-    CYGWIN* | MINGW32* | MSYS*)
-      curl -fsSL -o onessl.exe https://github.com/kubepack/onessl/releases/download/0.7.0/onessl-windows-amd64.exe
-      chmod +x onessl.exe
-      export ONESSL=./onessl.exe
-      ;;
-    *)
-      echo 'other OS'
-      ;;
-  esac
+        CYGWIN* | MINGW32* | MSYS*)
+            curl -fsSL -o onessl.exe https://github.com/kubepack/onessl/releases/download/0.7.0/onessl-windows-amd64.exe
+            chmod +x onessl.exe
+            export ONESSL=./onessl.exe
+            ;;
+        *)
+            echo 'other OS'
+            ;;
+    esac
 fi
 
 export VAULT_OPERATOR_NAMESPACE=default
@@ -64,57 +64,57 @@ export VAULT_OPERATOR_DOCKER_REGISTRY=kubevault
 export VAULT_OPERATOR_ENABLE_SUBRESOURCE=false
 
 while test $# -gt 0; do
-  case "$1" in
-    -n)
-      shift
-      if test $# -gt 0; then
-        export VAULT_OPERATOR_NAMESPACE=$1
-      else
-        echo "no namespace specified"
-        exit 1
-      fi
-      shift
-      ;;
-    --namespace*)
-      shift
-      if test $# -gt 0; then
-        export VAULT_OPERATOR_NAMESPACE=$1
-      else
-        echo "no namespace specified"
-        exit 1
-      fi
-      shift
-      ;;
-    --enable-webhook*)
-      val=$(echo $1 | sed -e 's/^[^=]*=//g')
-      if [ "$val" = "false" ]; then
-        export VAULT_OPERATOR_ENABLE_WEBHOOK=false
-      fi
-      shift
-      ;;
-    --docker-registry*)
-      export VAULT_OPERATOR_DOCKER_REGISTRY=$(echo $1 | sed -e 's/^[^=]*=//g')
-      shift
-      ;;
-    --test*)
-      val=$(echo $1 | sed -e 's/^[^=]*=//g')
-      if [ "$val" = "true" ]; then
-        export VAULT_OPERATOR_E2E_TEST=true
-      fi
-      shift
-      ;;
-    --enable-subresource*)
-      val=$(echo $1 | sed -e 's/^[^=]*=//g')
-      if [ "$val" = "true" ]; then
-        export VAULT_OPERATOR_ENABLE_SUBRESOURCE=true
-      fi
-      shift
-      ;;
-    *)
-      echo $1
-      exit 1
-      ;;
-  esac
+    case "$1" in
+        -n)
+            shift
+            if test $# -gt 0; then
+                export VAULT_OPERATOR_NAMESPACE=$1
+            else
+                echo "no namespace specified"
+                exit 1
+            fi
+            shift
+            ;;
+        --namespace*)
+            shift
+            if test $# -gt 0; then
+                export VAULT_OPERATOR_NAMESPACE=$1
+            else
+                echo "no namespace specified"
+                exit 1
+            fi
+            shift
+            ;;
+        --enable-webhook*)
+            val=$(echo $1 | sed -e 's/^[^=]*=//g')
+            if [ "$val" = "false" ]; then
+                export VAULT_OPERATOR_ENABLE_WEBHOOK=false
+            fi
+            shift
+            ;;
+        --docker-registry*)
+            export VAULT_OPERATOR_DOCKER_REGISTRY=$(echo $1 | sed -e 's/^[^=]*=//g')
+            shift
+            ;;
+        --test*)
+            val=$(echo $1 | sed -e 's/^[^=]*=//g')
+            if [ "$val" = "true" ]; then
+                export VAULT_OPERATOR_E2E_TEST=true
+            fi
+            shift
+            ;;
+        --enable-subresource*)
+            val=$(echo $1 | sed -e 's/^[^=]*=//g')
+            if [ "$val" = "true" ]; then
+                export VAULT_OPERATOR_ENABLE_SUBRESOURCE=true
+            fi
+            shift
+            ;;
+        *)
+            echo $1
+            exit 1
+            ;;
+    esac
 done
 
 # !!! WARNING !!! Never do this in prod cluster
@@ -131,14 +131,14 @@ rm -f ./onessl
 $REPO_ROOT/hack/make.py
 
 if [ "$VAULT_OPERATOR_E2E_TEST" = false ]; then # don't run operator while run this script from test
-vault-operator run --v=3 \
-  --secure-port=8443 \
-  --enable-validating-webhook="$VAULT_OPERATOR_ENABLE_WEBHOOK" \
-  --enable-mutating-webhook="$VAULT_OPERATOR_ENABLE_WEBHOOK" \
-  --kubeconfig="$HOME/.kube/config" \
-  --authorization-kubeconfig="$HOME/.kube/config" \
-  --authentication-kubeconfig="$HOME/.kube/config" \
-  --authentication-skip-lookup
+    vault-operator run --v=3 \
+        --secure-port=8443 \
+        --enable-validating-webhook="$VAULT_OPERATOR_ENABLE_WEBHOOK" \
+        --enable-mutating-webhook="$VAULT_OPERATOR_ENABLE_WEBHOOK" \
+        --kubeconfig="$HOME/.kube/config" \
+        --authorization-kubeconfig="$HOME/.kube/config" \
+        --authentication-kubeconfig="$HOME/.kube/config" \
+        --authentication-skip-lookup
 fi
 
 popd
--- hack/gendocs/make.sh
+++ hack/gendocs/make.sh
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 pushd $GOPATH/src/kubevault.dev/operator/hack/gendocs
 go run main.go
 popd
