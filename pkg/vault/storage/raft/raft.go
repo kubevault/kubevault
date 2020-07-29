d46f5251fcd9aa563173586ded88d5d769aa496a
Yoan Blanc <yoan.blanc@adfinis.com>
2020-07-29 11:40:51 +0200

    feat: activate some dev/debug options
    Signed-off-by: Yoan Blanc <yoan.blanc@adfinis.com>

--- pkg/vault/storage/raft/raft.go
+++ pkg/vault/storage/raft/raft.go
@@ -94,6 +94,10 @@ func (o *Options) Apply(pt *core.PodTemplateSpec) error {
 				},
 			},
 		},
+		core.EnvVar{
+			Name:  "VAULT_LOG_LEVEL",
+			Value: "debug",
+		},
 	)
 
 	// Configure the volume
